4356ada97aeb1dd04086df6dbed40d3ee9d20085
Rafal Kukulski <rafal.kukulski@gmail.com>
2019-05-28 11:33:04 +0200

    feat: send response directly from gather node

--- test/nodes/push/gather_spec.js
+++ test/nodes/push/gather_spec.js
@@ -2,6 +2,8 @@ var should = require("should");
 var helper = require("node-red-node-test-helper");
 var shared = require("../shared.js");
 var gatherNode = require("../../../nodes/push/gather.js");
+var res = require("../mocks.js");
+var fs = require('fs');
 
 helper.init(require.resolve('node-red'));
 
@@ -17,4 +19,69 @@ describe('gather node', function() {
   });
 
   shared.shouldLoadCorrectly(gatherNode, "gather");
+
+  it('should respond with proper XML', function(done) {
+    var flow = [{ id: "n1", type: "gather", onData: "http://example.com/dtmf" }];
+    var xml = fs.readFileSync('test/resources/xml/gather.xml', 'utf8');
+    helper.load(gatherNode, flow, function() {
+      var n1 = helper.getNode("n1");
+      n1.on("input", function(msg) {
+        should(msg.res._res.responseBody).be.eql(xml);
+        done();
+      });
+      n1.receive({ payload: '<call data>', res: res });
+    });
+  });
+
+  it('should respond with proper XML for custom maxDigits value', function(done) {
+    var flow = [{ id: "n1", type: "gather", onData: "http://example.com/dtmf", maxDigits: 3 }];
+    var xml = fs.readFileSync('test/resources/xml/gather_maxDigits.xml', 'utf8');
+    helper.load(gatherNode, flow, function() {
+      var n1 = helper.getNode("n1");
+      n1.on("input", function(msg) {
+        should(msg.res._res.responseBody).be.eql(xml);
+        done();
+      });
+      n1.receive({ payload: '<call data>', res: res });
+    });
+  });
+
+  it('should respond with proper XML for custom timeout value', function(done) {
+    var flow = [{ id: "n1", type: "gather", onData: "http://example.com/dtmf", timeout: 10000 }];
+    var xml = fs.readFileSync('test/resources/xml/gather_timeout.xml', 'utf8');
+    helper.load(gatherNode, flow, function() {
+      var n1 = helper.getNode("n1");
+      n1.on("input", function(msg) {
+        should(msg.res._res.responseBody).be.eql(xml);
+        done();
+      });
+      n1.receive({ payload: '<call data>', res: res });
+    });
+  });
+
+  it('should respond with proper XML for onHangup callback', function(done) {
+    var flow = [{ id: "n1", type: "gather", onData: "http://example.com/dtmf", onHangup: "http://example.com/callback" }];
+    var xml = fs.readFileSync('test/resources/xml/gather_onHangup.xml', 'utf8');
+    helper.load(gatherNode, flow, function() {
+      var n1 = helper.getNode("n1");
+      n1.on("input", function(msg) {
+        should(msg.res._res.responseBody).be.eql(xml);
+        done();
+      });
+      n1.receive({ payload: '<call data>', res: res });
+    });
+  });
+
+  it('should respond with proper XML for playUrl', function(done) {
+    var flow = [{ id: "n1", type: "gather", onData: "http://example.com/dtmf", playUrl: "http://example.com/example.wav" }];
+    var xml = fs.readFileSync('test/resources/xml/gather_playUrl.xml', 'utf8');
+    helper.load(gatherNode, flow, function() {
+      var n1 = helper.getNode("n1");
+      n1.on("input", function(msg) {
+        should(msg.res._res.responseBody).be.eql(xml);
+        done();
+      });
+      n1.receive({ payload: '<call data>', res: res });
+    });
+  });
 });
