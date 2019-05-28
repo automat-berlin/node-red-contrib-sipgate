25b0f907080aae25dad3fa4ef4efa2f758f5ac06
Rafal Kukulski <rafal.kukulski@gmail.com>
2019-05-28 11:32:07 +0200

    feat: send response directly from reject node

--- test/nodes/push/reject_spec.js
+++ test/nodes/push/reject_spec.js
@@ -2,6 +2,8 @@ var should = require("should");
 var helper = require("node-red-node-test-helper");
 var shared = require("../shared.js");
 var rejectNode = require("../../../nodes/push/reject.js");
+var res = require("../mocks.js");
+var fs = require('fs');
 
 helper.init(require.resolve('node-red'));
 
@@ -17,4 +19,21 @@ describe('reject node', function() {
   });
 
   shared.shouldLoadCorrectly(rejectNode, "reject");
+
+  var reasons = ["rejected", "busy"];
+
+  reasons.forEach(function(reason) {
+    it('should respond with proper XML for reason ' + reason, function(done) {
+      var flow = [{ id: "n1", type: "reject", reason: reason }];
+      var xml = fs.readFileSync(`test/resources/xml/reject_${reason}.xml`, 'utf8');
+      helper.load(rejectNode, flow, function() {
+        var n1 = helper.getNode("n1");
+        n1.on("input", function(msg) {
+          should(msg.res._res.responseBody).be.eql(xml);
+          done();
+        });
+        n1.receive({ payload: '<call data>', res: res });
+      });
+    });
+  });
 });
