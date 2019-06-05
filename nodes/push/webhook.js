1f53aa5147efbc364c77993f80dad7912229d55d
Rafal Kukulski <rafal.kukulski@gmail.com>
2019-06-05 09:58:16 +0200

    feat: add webhook node

---
+++ nodes/push/webhook.js
@@ -0,0 +1,45 @@
+module.exports = function(RED) {
+  'use strict';
+  var bodyParser = require('body-parser');
+
+  function WebhookNode(config) {
+    RED.nodes.createNode(this, config);
+    if (!config.url) {
+      this.warn('missing path');
+      return;
+    }
+    this.url = config.url;
+    if (this.url[0] !== '/') {
+      this.url = '/' + this.url;
+    }
+    this.method = 'post';
+
+    var node = this;
+
+    this.errorHandler = function(err, req, res, next) {
+      node.warn(err);
+      res.sendStatus(500);
+    };
+
+    this.callback = function(req, res) {
+      var msgid = RED.util.generateId();
+      res._msgid = msgid;
+      node.send({ _msgid: msgid, req: req, res: { _res: res }, payload: req.body });
+    };
+
+    var maxApiRequestSize = RED.settings.apiMaxLength || '5mb';
+    var urlencParser = bodyParser.urlencoded({ limit: maxApiRequestSize, extended: true });
+
+    RED.httpNode.post(this.url, urlencParser, this.callback, this.errorHandler);
+
+    this.on('close', function() {
+      var node = this;
+      RED.httpNode._router.stack.forEach(function(route, i, routes) {
+        if (route.route && route.route.path === node.url && route.route.methods[node.method]) {
+          routes.splice(i, 1);
+        }
+      });
+    });
+  }
+  RED.nodes.registerType('webhook', WebhookNode);
+}
---
+++ test/nodes/push/webhook_spec.js
@@ -0,0 +1,24 @@
+var should = require('should');
+var helper = require('node-red-node-test-helper');
+var shared = require('../shared.js');
+var webhookNode = require('../../../nodes/push/webhook.js');
+
+helper.init(require.resolve('node-red'));
+
+describe('webhook node', function() {
+
+  beforeEach(function(done) {
+    helper.startServer(done);
+  });
+
+  afterEach(function(done) {
+    helper.unload();
+    helper.stopServer(done);
+  });
+
+  shared.shouldLoadCorrectly(webhookNode, 'webhook');
+
+  it('should create POST endpoint');
+  it('should remove POST endpoint on close');
+  it('should send received payload to the next node');
+});
