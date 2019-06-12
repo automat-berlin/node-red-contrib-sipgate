656b0aa5d67469b53480d06fddad43141cf05e37
Rafal Kukulski <rafal.kukulski@gmail.com>
2019-06-12 18:11:16 +0200

    feat: add account and hangup-call nodes

---
+++ nodes/rtcm/hangup-call.js
@@ -0,0 +1,22 @@
+module.exports = function(RED) {
+  'use strict';
+  var request = require('request');
+
+  function HangupCallNode(config) {
+    RED.nodes.createNode(this, config);
+    this.account = RED.nodes.getNode(config.account);
+    var node = this;
+
+    node.on('input', function(msg) {
+      var credentialString = node.account.getCredentialString();
+      var opts = {};
+      opts.url = 'https://api.sipgate.com/v2/calls/' + msg.payload.callId;
+      opts.method = 'DELETE';
+      opts.headers = {};
+      opts.headers['Authorization'] = 'Basic ' + credentialString;
+      opts.headers['Content-Type'] = 'application/json';
+      request(opts);
+    });
+  }
+  RED.nodes.registerType('hangup-call', HangupCallNode);
+}
