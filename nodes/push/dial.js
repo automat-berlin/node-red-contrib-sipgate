41d227106ded963aedfe5e2d7ada511995de3232
Rafal Kukulski <rafal.kukulski@gmail.com>
2019-06-28 08:52:13 +0200

    feat: handle onAnswer/onHangup callbacks in dial node

--- nodes/push/dial.js
+++ nodes/push/dial.js
@@ -1,6 +1,8 @@
 module.exports = function(RED) {
   "use strict";
   var xmlbuilder = require('xmlbuilder');
+  var url = require('url');
+  var bodyParser = require('body-parser');
 
   function DialNode(config) {
     RED.nodes.createNode(this, config);
@@ -8,10 +10,15 @@ module.exports = function(RED) {
     this.numbers = config.numbers;
     this.anonymous = config.anonymous;
     this.callerId = config.callerId;
-    this.onHangup = config.onHangup;
     this.onAnswer = config.onAnswer;
+    this.onHangup = config.onHangup;
+    this.outputs = config.outputs;
+    this.callbackUrl = '/callback-' + randomId();
+    this.method = 'post';
     var node = this;
+
     node.on('input', function(msg) {
+      var absoluteCallbackUrl = url.resolve(this.context().global.get('baseUrl'), node.callbackUrl);
       var root = xmlbuilder.create('Response').dec('1.0', 'UTF-8');
       var xml = root.ele('Dial');
       if (node.anonymous) {
@@ -27,15 +34,48 @@ module.exports = function(RED) {
       } else if (node.target == 'voicemail') {
         xml.ele('Voicemail');
       }
-      if (node.onHangup) {
-        root.att('onHangup', node.onHangup);
-      }
       if (node.onAnswer) {
-        root.att('onAnswer', node.onAnswer);
+        root.att('onAnswer', absoluteCallbackUrl);
+      }
+      if (node.onHangup) {
+        root.att('onHangup', absoluteCallbackUrl);
       }
       msg.payload = xml.end({ pretty: true, indent: '    ' });
       msg.res._res.set('Content-Type', 'application/xml');
       msg.res._res.status(200).send(msg.payload);
+    });
+
+    function randomId() {
+      return Math.floor(Math.random() * 1000000);
+    }
+
+    this.errorHandler = function(err, req, res, next) {
+      node.warn(err);
+      res.sendStatus(500);
+    };
+
+    this.callback = function(req, res) {
+      var msgid = RED.util.generateId();
+      res._msgid = msgid;
+      var msg = { _msgid: msgid, req: req, res: { _res: res }, payload: req.body };
+      var envelope = [];
+      (req.body.event == 'answer' && node.onAnswer) ? envelope.push(msg) : envelope.push(null);
+      (req.body.event == 'hangup' && node.onHangup) ? envelope.push(msg) : envelope.push(null);
+      node.send(envelope);
+    };
+
+    var maxApiRequestSize = RED.settings.apiMaxLength || '5mb';
+    var urlencParser = bodyParser.urlencoded({ limit: maxApiRequestSize, extended: true });
+
+    RED.httpNode.post(this.callbackUrl, urlencParser, this.callback, this.errorHandler);
+
+    this.on('close', function() {
+      var node = this;
+      RED.httpNode._router.stack.forEach(function(route, i, routes) {
+        if (route.route && route.route.path === node.callbackUrl && route.route.methods[node.method]) {
+          routes.splice(i, 1);
+        }
+      });
     });
   }
   RED.nodes.registerType("dial", DialNode);
--- test/nodes/push/dial_spec.js
+++ test/nodes/push/dial_spec.js
@@ -25,6 +25,7 @@ describe('dial node', function() {
     var xml = fs.readFileSync('test/resources/xml/dial_number.xml', 'utf8');
     helper.load(dialNode, flow, function() {
       var n1 = helper.getNode("n1");
+      n1.context().global.set('baseUrl', 'http://example.com');
       n1.on("input", function(msg) {
         should(msg.res._res.responseBody).be.eql(xml);
         done();
@@ -38,6 +39,7 @@ describe('dial node', function() {
     var xml = fs.readFileSync('test/resources/xml/dial_voicemail.xml', 'utf8');
     helper.load(dialNode, flow, function() {
       var n1 = helper.getNode("n1");
+      n1.context().global.set('baseUrl', 'http://example.com');
       n1.on("input", function(msg) {
         should(msg.res._res.responseBody).be.eql(xml);
         done();
@@ -51,6 +53,7 @@ describe('dial node', function() {
     var xml = fs.readFileSync('test/resources/xml/dial_anonymous.xml', 'utf8');
     helper.load(dialNode, flow, function() {
       var n1 = helper.getNode("n1");
+      n1.context().global.set('baseUrl', 'http://example.com');
       n1.on("input", function(msg) {
         should(msg.res._res.responseBody).be.eql(xml);
         done();
@@ -64,6 +67,7 @@ describe('dial node', function() {
     var xml = fs.readFileSync('test/resources/xml/dial_callerId.xml', 'utf8');
     helper.load(dialNode, flow, function() {
       var n1 = helper.getNode("n1");
+      n1.context().global.set('baseUrl', 'http://example.com');
       n1.on("input", function(msg) {
         should(msg.res._res.responseBody).be.eql(xml);
         done();
@@ -77,6 +81,7 @@ describe('dial node', function() {
     var xml = fs.readFileSync('test/resources/xml/dial_two_numbers.xml', 'utf8');
     helper.load(dialNode, flow, function() {
       var n1 = helper.getNode("n1");
+      n1.context().global.set('baseUrl', 'http://example.com');
       n1.on("input", function(msg) {
         should(msg.res._res.responseBody).be.eql(xml);
         done();
@@ -85,29 +90,21 @@ describe('dial node', function() {
     });
   });
 
-  it('should respond with proper XML for target number and onHangup callback', function(done) {
-    var flow = [{ id: "n1", type: "dial", target: 'number', numbers: [{ number: '4915799912345' }], onHangup: "http://example.com/callback" }];
-    var xml = fs.readFileSync('test/resources/xml/dial_number_onHangup.xml', 'utf8');
+  it('should respond with proper XML for target number and onAnswer/onHangup callbacks', function(done) {
+    var flow = [{ id: "n1", type: "dial", target: 'number', numbers: [{ number: '4915799912345' }], onAnswer: true, onHangup: true }];
+    var xml = fs.readFileSync('test/resources/xml/dial_number_callbacks.xml', 'utf8');
     helper.load(dialNode, flow, function() {
       var n1 = helper.getNode("n1");
+      n1.context().global.set('baseUrl', 'http://example.com');
       n1.on("input", function(msg) {
-        should(msg.res._res.responseBody).be.eql(xml);
+        should(msg.res._res.responseBody).be.eql(xml.replace(/\${callbackUrl}/g, n1.callbackUrl));
         done();
       });
       n1.receive({ payload: '<call data>', res: res });
     });
   });
 
-  it('should respond with proper XML for target number and onAnswer callback', function(done) {
-    var flow = [{ id: "n1", type: "dial", target: 'number', numbers: [{ number: '4915799912345' }], onAnswer: "http://example.com/callback" }];
-    var xml = fs.readFileSync('test/resources/xml/dial_number_onAnswer.xml', 'utf8');
-    helper.load(dialNode, flow, function() {
-      var n1 = helper.getNode("n1");
-      n1.on("input", function(msg) {
-        should(msg.res._res.responseBody).be.eql(xml);
-        done();
-      });
-      n1.receive({ payload: '<call data>', res: res });
-    });
-  });
+  it('should create callback endpoint');
+  it('should remove callback endpoint on close');
+  it('should send received callback payload to the next node');
 });
