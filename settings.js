725b584721f88128c6aff1b68c525eee31e017a7
Rafal Kukulski <rafal.kukulski@gmail.com>
2019-08-06 15:14:09 +0200

    chore: generate baseUrl for localhost if APP_NAME undefined

--- settings.js
+++ settings.js
@@ -1,6 +1,16 @@
+var baseUrl, uiPort;
+
+uiPort = process.env.PORT || 1880;
+
+if (process.env.APP_NAME) {
+  baseUrl = 'https://' + process.env.APP_NAME + '.herokuapp.com';
+} else {
+  baseUrl = 'http://localhost:' + uiPort;
+}
+
 module.exports = {
   // the tcp port that the Node-RED web server is listening on
-  uiPort: process.env.PORT || 1880,
+  uiPort: uiPort,
 
   // Node-RED scans the `nodes` directory in the userDir to find local node files
   userDir: __dirname,
@@ -20,6 +30,6 @@ module.exports = {
   // sets global context data
   functionGlobalContext: {
     // required to generate sipgate callbacks URLs
-    baseUrl: 'https://' + process.env.APP_NAME + '.herokuapp.com',
+    baseUrl: baseUrl,
   },
 };
