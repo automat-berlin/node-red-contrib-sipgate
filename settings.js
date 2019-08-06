492fcc46d5e256e89be2e3ce866072b053ad60c8
Rafal Kukulski <rafal.kukulski@gmail.com>
2019-08-06 11:28:11 +0200

    chore: set userDir in settings.js

--- settings.js
+++ settings.js
@@ -1,4 +1,7 @@
 module.exports = {
   // the tcp port that the Node-RED web server is listening on
   uiPort: process.env.PORT || 1880,
+
+  // Node-RED scans the `nodes` directory in the userDir to find local node files
+  userDir: __dirname,
 };
